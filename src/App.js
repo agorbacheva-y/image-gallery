f43bbe0d263d673d45132ea8499e56350d621e56
agorbacheva-y <agorbacheva.y@gmail.com>
2023-05-02 14:09:21 +0200

    code-along pt1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import { useState } from "react";
+import Card from "./Card.js";
+
 function App() {
+  const [ images, setImages ] = useState([
+    { url: "https://source.unsplash.com/random/1" },
+    { url: "https://source.unsplash.com/random/2" },
+    { url: "https://source.unsplash.com/random/3" },
+    { url: "https://source.unsplash.com/random/4" },
+    { url: "https://source.unsplash.com/random/5" },
+    { url: "https://source.unsplash.com/random/6" }
+  ]);
+
   return (
-    <div >
-      image gallery
-    </div>
+    <section className="hero">
+      <div className="cardContainer">
+        <Card images={images} />
+      </div>
+
+      <div className="btnContainer">
+        <button>Add</button>
+        <button>Remove</button>
+      </div>
+    </section>
   );
 }
 
 export default App;
+
+// fetch images from API
+// define what states we need. ask what do you want to manipulate?
+// --> need state for images
+
+/*
+1. create image state
+2. create jsx that returns
+3. reusable component that is responsible for showing one image
+4. make sure we grab images from unsplash
+5, hard code first 5-6 images with url from unsplash
+6. in add image method, we should add correct url to get random image
+7, create function to remove image
+*/
---
+++ src/Card.js
@@ -0,0 +1,19 @@
+const Card = ({ images }) => {
+  return (
+    <>
+      {images.map((img, i) => {
+        <div  className="card" 
+              key={i} 
+              style={{ background: `url("${img.url}") no-repeat center/cover` }}>
+        </div>
+      })}
+    </>
+  );
+};
+
+export default Card;
+
+// reusable component to display one image
+// images added into array. iterate thru array to display one image from array
+// pass images array as prop (component expects to use this prop)
+// no functionality, no state here
