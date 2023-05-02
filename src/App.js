de9008c0c1d26392225837ced744ec757b3a1492
agorbacheva-y <agorbacheva.y@gmail.com>
2023-05-02 14:31:03 +0200

    add comments

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import Card from "./Card.js";
 
 function App() {
   const [ images, setImages ] = useState([
-    { url: "https://source.unsplash.com/random/1" },
-    { url: "https://source.unsplash.com/random/2" },
-    { url: "https://source.unsplash.com/random/3" },
-    { url: "https://source.unsplash.com/random/4" },
-    { url: "https://source.unsplash.com/random/5" },
-    { url: "https://source.unsplash.com/random/6" }
+    { url: "http://source.unsplash.com/random/1" },
+    { url: "http://source.unsplash.com/random/2" },
+    { url: "http://source.unsplash.com/random/3" },
+    { url: "http://source.unsplash.com/random/4" },
+    { url: "http://source.unsplash.com/random/5" },
+    { url: "http://source.unsplash.com/random/6" }
   ]);
 
   const getNewImages = () => {
-    setImages([...images, {
-      url: `https://source.unsplash.com/random/${Math.floor(Math.random()*100)}`,
-    }]);
+    setImages([...images, 
+      {
+        url: `http://source.unsplash.com/random/${Math.floor(Math.random() * 100)}`,
+      },
+    ]);
   };
 
   const removeImage = () => {
     setImages(images.slice(0, -1));
   };
+  // we are saying that our state (images) will images -1
 
   return (
     <section className="hero">
--- src/Card.js
+++ src/Card.js
@@ -3,10 +3,11 @@ const Card = ({ images }) => {
     // no functionality, no state here
     <>
       {images.map((img, i) => {
-        <div  className="card" 
-              key={i} 
-              style={{ background: `url("${img.url}") no-repeat center/cover` }}>
-        </div>
+        <div  
+          className="card" 
+          key={i} 
+          style={{ background: `url("${img.url}") no-repeat center/cover` }}
+          ></div>
       })}
     </>
   );
